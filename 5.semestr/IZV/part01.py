b8e0fa74f2627cdc876c28cf2d03a536c5af0d28
michkr123 <krysac123@seznam.cz>
2024-10-17 18:34:32 +0200

    IMP hello world - didnt test, need better cable IZV - still trying to generate the graph

--- 5.semestr/IZV/part01.py
+++ 5.semestr/IZV/part01.py
@@ -26,25 +26,31 @@ def generate_graph(a: List[float], show_figure: bool = False, save_path: str | N
     x = np.linspace(0, 6 * np.pi, 1000)
 
     ax = plt.gca()
+    ax.xaxis.set_major_formatter(FuncFormatter(format_func))
+
+    xticks = np.arange(0, 6.5 * np.pi, 0.5 * np.pi)
+    ax.set_xticks(xticks)
+    ax.set_xlim(0,6*np.pi)
+    yticks = np.arange(-50, 50)
+    ax.set_yticks([-40, -20, 0, 20, 40])
+    #ax.set_yticks(yticks)
+
 
     for coeff in a:
-        plt.plot(x, coeff * np.sin(x), label=f"$y_{coeff}(x)$")
-        plt.fill_between(x, coeff * np.sin(x), alpha=0.1)
+        plt.plot(x, np.power(coeff, 2) * np.sin(x), label=f"$y_{coeff}(x)$")
+        plt.fill_between(x, np.power(coeff, 2) * np.sin(x), alpha=0.1)
 
-    # Customize the plot
     plt.xlabel("X")
     plt.ylabel("$f_{a}(x)$")
     ax.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3)
 
-    # Save the figure if a path is provided
     if save_path:
-        plt.savefig(save_path)  # Save the figure to the specified path
+        plt.savefig(save_path)
 
-    # Show the figure if requested
     if show_figure:
-        plt.show()  # Display the graph
+        plt.show()
 
-    plt.close()  # Close the plot to free up memory
+    plt.close()
 
 
 def generate_sinus(show_figure: bool = False, save_path: str | None = None):
@@ -54,10 +60,20 @@ def generate_sinus(show_figure: bool = False, save_path: str | None = None):
 def download_data() -> Dict[str, List[Any]]:
     pass
 
+#def format_func(value, tick_number):
+#    N = int(np.round(2 * value / np.pi))
+#
+#    if (N == 0) :
+#        return fr"${N}$"
+#    elif (N % 1 == 0):
+#        return f"${N}\pi$"
+#    else :
+#        return f"$\frac{{N}}{2}\pi$"
+
 def format_func(value, tick_number):
     N = int(np.round(2 * value / np.pi))
-    if N>=0 and N < 6:
-        return {0:"0", 1: r"$\piz /2$", 2: r"$\pi$"}[N]
+    if N>=0 and N < 3:
+        return {0:"0", 1: r"$\pi/2$", 2: r"$\pi$"}[N]
     elif N % 2 > 0:
         return f"${N}\pi/2$"
     else:
