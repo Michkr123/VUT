c7cac5fdb9acfbcf889b6ced0237e422fd4b712d
catohas <petr.hron04@gmail.com>
2024-11-13 02:57:32 +0100

    Move event creation to admin page

--- frontend/src/AdminPage.tsx
+++ frontend/src/AdminPage.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+
 const AdminPage = () => (
   <div className="container mx-auto px-4 py-8">
     <div className="bg-white rounded-lg shadow p-6">
-      <p className="text-gray-600">Stránka admina zde</p>
+      <p className="text-2xl font-bold mb-3">Správa akcí:</p>
+      <Button variant="contained">
+        <Link to="/admin/new_event">Vytvořit novou akci</Link>
+      </Button>
     </div>
   </div>
 );
