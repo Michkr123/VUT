0f43d2dff3cdde514993b5da794850cf8d5fa0ef
catohas <petr.hron04@gmail.com>
2024-11-13 02:56:06 +0100

    Add user context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,26 +14,27 @@ import ProfileChangePage from "./ProfileChangePage";
 import AddReviewPage from "./AddReviewPage";
 import ProfileEditPage from "./ProfileEditPage";
 
-const login = "admin";
+import { UserProvider } from './UserContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* Routování mezi jednotlivými časti aplikace */}
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Header /> {/* Hlavička je zde aby se zobrazila v jakékoliv části aplikace */}
-        <Routes>
-          <Route path="/" element={<EventsPage />} />
-          <Route path="/event/:id" element={<EventDetails />} />
-          <Route path="/worker" element={<WorkerPage />} />
-          <Route path="/admin" element={<AdminPage />} />
-          <Route path="/admin/new_event" element={<NewEvent />} />
-          <Route path="/profile" element={<ProfilePage login={login} />} />
-          <Route path="/profilechange" element={<ProfileChangePage login={login} />} />
-          <Route path="/event/:id/addreview" element={<AddReviewPage login={login} />} />
-          <Route path="/profile/edit" element={<ProfileEditPage login={login} />} />
-        </Routes>
-      </div>
-    </Router>
+    <UserProvider> {/* Toto umožňuje získání informací o uživateli v jakékoliv části aplikace */}
+      <Router>
+        <div className="min-h-screen bg-gray-50">
+          <Header /> {/* Hlavička je zde aby se zobrazila v jakékoliv části aplikace */}
+          <Routes>
+            <Route path="/" element={<EventsPage />} />
+            <Route path="/event/:id" element={<EventDetails />} />
+            <Route path="/worker" element={<WorkerPage />} />
+            <Route path="/admin" element={<AdminPage />} />
+            <Route path="/admin/new_event" element={<NewEvent />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/profilechange" element={<ProfileChangePage />} />
+            <Route path="/event/:id/addreview" element={<AddReviewPage />} />
+            <Route path="/profile/edit" element={<ProfileEditPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </UserProvider>
   </StrictMode>,
 )
