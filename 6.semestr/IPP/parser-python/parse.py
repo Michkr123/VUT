a88d11a6a5b37b2773b6be20710125663270a4ee
michkr123 <krysac123@seznam.cz>
2025-02-17 12:22:12 +0100

    Some more files

---
+++ 6.semestr/IPP/parser-python/parse.py
@@ -0,0 +1,13 @@
+### IPP projekt
+### autor: Michálek Kryštof
+### login: xmicha94
+
+import sys
+
+#################### HELP ####################
+if len(sys.argv) == 2:
+    if (sys.argv[1] == "--help"):
+        print("napoveda")
+        sys.exit(0)
+    else:
+        sys.exit(10)
